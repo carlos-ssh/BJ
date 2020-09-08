02d84a8ffacacf138289d21262c6a27dae5cd8f4
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-08 18:05:59 -0500

    Instructions created.

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts "Hello World"
+=begin
+ 1.- Repartir las cartas
+ 
+ 2.- Preguntar al usuario si desea carta o se planta
+        Si pide carta:
+            Si se pasa, ir al punto 4
+            De lo contrario volver al punto 2        
+        Si se planta:
+            Ir al punto 3
+
+ 3.- Determinar el valor que tiene el repartidor
+        Si ese valor < 17, entregarse otra carta
+            Si se pasa ir al punto 4
+            De lo contrario, volver al punto 3.
+        De lo contrario:
+            Ir al punto 4 .
+
+ 4.- Determina el ganador
+        Sí el usuario se pasó, gana el repartidor.
+        Sí el repartidor se pasó, gana el usuario.
+        Sí el repartidor tiene mas que el usuario, gana el repartidor.
+        De lo contrario gana el usuario.
+
+ 5.- Volver al punto uno (Nuevo juego.)   
+
+=end
