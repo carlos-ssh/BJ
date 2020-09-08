c71f5f8eb18df34514c5ba1686bf227335426cd1
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-08 18:34:50 -0500

    Class create:
    Class card declaring the kind of cards and deck declaring kind and value.

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 =begin
  1.- Repartir las cartas
- 
+
  2.- Preguntar al usuario si desea carta o se planta
         Si pide carta:
             Si se pasa, ir al punto 4
@@ -24,3 +24,45 @@
  5.- Volver al punto uno (Nuevo juego.)   
 
 =end
+
+#Como representamos una carta (Figura y valor)
+#Como representamos una baraja compuesta de 52 cartas
+#Como representamos las cartas que tiene el jugador y el repartidor
+
+class Card
+    attr_reader :suit, :value
+
+    def initialize(suit, value)
+        @suit = suit
+        @value = value
+    end
+
+    def value
+        return 10 if ["J", "Q", "K"].include?(value)
+        return 11 if @value == "A"
+        return @value
+    end
+
+end
+
+class Deck
+    attr_writer :cards
+
+    def initialize
+        @cards = []
+        build_cards
+    end
+
+    private
+    def build_cards
+        [:clubs, :diamonds, :spades, :hearts].each do |suit|
+            (2..10).each do |number|
+                @cards << Card.new(suit, number)
+            end
+            ["J", "Q", "K", "A"].each do |face|
+                @cards << Card.new(suit, face)
+            end
+        end
+        @cards.shuffle!
+    end
+end
